50e86346a8a888dd0326f670c5f70acf2b6a3dc5
caokimnhat <zjackanderson@gmail.com>
2022-01-06 23:12:05 +0700

    update main

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const route = require('./routes/index');
+
+const PUBLIC_PATH = path.join(__dirname, '../public');
+app.use(express.static(PUBLIC_PATH));
 
 const app = express();
 
@@ -11,6 +16,13 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({ origin: "*" }));
 
 const PORT = process.env.PORT || 6788;
+
+require('./middlewares/express-session')(app);
+require('./middlewares/cookie-parser')(app);
+require('./middlewares/express-handlebars')(app);
+require('./middlewares/passport')(app);
+
+route(app);
 
 app.listen(PORT, function () {
   console.log(`Running port ${PORT}!!!!`);
