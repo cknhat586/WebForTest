dbad9462d492efb25d49fb3504e8931b571dea2d
caokimnhat <zjackanderson@gmail.com>
2022-01-06 22:58:10 +0700

    .

---
+++ server/controllers/SiteController.js
@@ -0,0 +1,12 @@
+class SiteController {
+
+    indexGET(req, res) {
+        res.render('login');
+    }
+
+    indexPOST(req, res) {
+        res.render('login');
+    }
+}
+
+module.exports = new SiteController;
---
+++ server/routes/sites.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const siteController = require('../controllers/SiteController');
+
+router.get('/', siteController.indexGET);
+
+router.post('/', siteController.indexPOST);
+
+
+module.exports = router;
