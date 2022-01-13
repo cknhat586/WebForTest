59c1e5340ac99ed776ba565b308b45925d251759
ttvuhcmus <ttvuhcmus@gmail.com>
2022-01-13 23:22:57 +0700

    [UPDATE] Setup main layout and dashboard layout

--- server/controllers/SiteController.js
+++ server/controllers/SiteController.js
@@ -7,6 +7,26 @@ class SiteController {
     indexPOST(req, res) {
         res.render('login');
     }
+
+    dashboard(req, res) {
+        res.render('dashboard', { layout: "dashboard", activeDashboard: true} );
+    }
+
+    user(req, res) {
+        res.render('user', { layout: "dashboard", activeUser: true });
+    }
+
+    medicine(req, res) {
+        res.render('medicine', { layout: "dashboard", activeMedicine: true });
+    }
+
+    location(req, res) {
+        res.render('location', { layout: "dashboard", activeLocation: true });
+    }
+
+    food(req, res) {
+        res.render('food', { layout: "dashboard", activeFood: true });
+    }   
 }
 
 module.exports = new SiteController;
--- server/routes/sites.js
+++ server/routes/sites.js
@@ -5,6 +5,16 @@ const siteController = require('../controllers/SiteController');
 
 router.get('/', siteController.indexGET);
 
+router.get('/dashboard', siteController.dashboard);
+
+router.get('/user', siteController.user);
+
+router.get('/location', siteController.location);
+
+router.get('/food', siteController.food);
+
+router.get('/medicine', siteController.medicine);
+
 router.post('/', siteController.indexPOST);
 
 
