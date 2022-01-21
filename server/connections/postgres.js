55cec288cbcaead25b7a5a93c5ec552500c5d6bf
DESKTOP-5EON1NH\XV11 <vinhvx3@fpt.com.vn>
2022-01-21 13:04:50 +0700

    wallets

--- server/connections/postgres.js
+++ server/connections/postgres.js
@@ -1,43 +1,58 @@
-const config = require('../config/db_config');
-const pg = require('pg-promise')(config.initOptions);
+const config = require("../config/db_config");
+const pg = require("pg-promise")(config.initOptions);
 
 const connect_string = config.connect_string;
 const schema = config.schema;
 
 const db = pg(connect_string);
 
-exports.load = async tbName => {
-    const table = new pg.helpers.TableName({ table: tbName, schema: schema});
-    const qStr= pg.as.format('SELECT * FROM $1', table);
+exports.load = async (tbName) => {
+  const table = new pg.helpers.TableName({ table: tbName, schema: schema });
+  const qStr = pg.as.format("SELECT * FROM $1", table);
 
-    try {
-        const res = await db.any(qStr);
-        return res;
-    } catch (err) {
-        console.log('ERROR: ', err);
-    }
+  try {
+    const res = await db.any(qStr);
+    return res;
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
 };
 
 exports.get = async (tbName, fieldName, value) => {
-    const table = new pg.helpers.TableName({ table: tbName, schema: schema});
-    const qStr= pg.as.format(`SELECT * FROM $1 WHERE "${fieldName}"='${value}'`, table);
+  const table = new pg.helpers.TableName({ table: tbName, schema: schema });
+  const qStr = pg.as.format(
+    `SELECT * FROM $1 WHERE "${fieldName}"='${value}'`,
+    table
+  );
 
-    try {
-        const res = await db.any(qStr);
-        return res;
-    } catch (err) {
-        console.log('ERROR: ', err);
-    }
+  try {
+    const res = await db.any(qStr);
+    return res;
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
 };
 
 exports.add = async (tbName, entity) => {
-    const table = new pg.helpers.TableName({ table: tbName, schema: schema});
-    const qStr = pg.helpers.insert(entity, null, table) + ' RETURNING *';
+  const table = new pg.helpers.TableName({ table: tbName, schema: schema });
+  const qStr = pg.helpers.insert(entity, null, table) + " RETURNING *";
 
-    try {
-        const res = await db.one(qStr);
-        return res;
-    } catch (err) {
-        console.log('ERROR: ', err);
-    }
+  try {
+    const res = await db.one(qStr);
+    return res;
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
+};
+
+exports.update = async (tbName, fieldName, value, data) => {
+  const condition = pgp.as.format(` WHERE '${fieldName}' = '${value}'`, data);
+  const qStr = pgp.helpers.update(data, ["555555"], tbName) + condition;
+
+  try {
+    const res = await db.one(qStr);
+    return res;
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
 };
